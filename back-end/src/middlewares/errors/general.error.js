5ab03b6da8638509fdbc6cfdc01396c7918deee7
Gabrielle-Murat <belle.murat@gmail.com>
2022-09-21 19:21:40 -0300

    refactor: Modifica o tratamento genérico de erro

--- back-end/src/middlewares/errors/general.error.js
+++ back-end/src/middlewares/errors/general.error.js
@@ -1,20 +1,31 @@
-module.exports = (err, _req, res, _next) => {
-  const { name, message } = err;
+const errorTreatment = async (err, _req, res, next) => {
+  const { status, message } = err;
 
-  switch (name) {
-    case 'not found':
-      res.status(404).json({ message });
-      break;
-    case 'non existant token':
-      res.status(401).json({ message });
-      break;
-    case 'invalid token':
-      res.status(401).json({ message });
-      break;
-    case 'invalid login':
-      res.status(404).json({ message });
-      break;
-    default:
-      res.status(500).json({ message: 'Internal error' });
-  }
+  res.status(status).json({ message });
+
+  next();
 };
+
+module.exports = { errorTreatment };
+
+// module.exports = (err, _req, res, _next) => {
+//   const { name, message } = err;
+//   console.log(err);
+
+//   switch (name) {
+//     case 'not found':
+//       res.status(404).json({ message });
+//       break;
+//     case 'non existant token':
+//       res.status(401).json({ message });
+//       break;
+//     case 'invalid token':
+//       res.status(401).json({ message });
+//       break;
+//     case 'invalid login':
+//       res.status(404).json({ message });
+//       break;
+//     default:
+//       res.status(500).json({ message: 'Internal error' });
+//   }
+// };
