1559d6f7710c2627a659e839b41fb3fedfe75a03
Gabrielle-Murat <belle.murat@gmail.com>
2022-09-21 19:22:13 -0300

    feat: Cria uma classe herdeira para customizar erros

---
+++ back-end/src/middlewares/errors/custom.error.js
@@ -0,0 +1,12 @@
+class CustomError extends Error {
+  status;
+  message;
+
+  constructor(status, message) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+}
+
+module.exports = CustomError;
