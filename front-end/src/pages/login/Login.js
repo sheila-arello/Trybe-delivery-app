37a4f67197b5b2c3c4db861347050ff2342a419c
vitória Holanda <86388276+vitoriacst@users.noreply.github.com>
2022-09-22 17:45:19 -0300

    validação dos inputs da página de login

--- front-end/src/pages/login/Login.js
+++ front-end/src/pages/login/Login.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import caneca from '../../images/caneca.svg';
 
 function Login() {
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+  const [button, setButton] = useState(true);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setLogin({ ...login, [name]: value });
+  };
+
+  const validateLogin = () => {
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/gi;
+    const isEmailValid = emailRegex.test(login.email);
+    const lengthPassword = 6;
+    const isPasswordValid = login.password.length >= lengthPassword;
+
+    if (isEmailValid && isPasswordValid) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    validateLogin();
+  }, [login]);
+
   return (
     <div>
       <img src={ caneca } alt="caneca" />
@@ -9,42 +36,49 @@ function Login() {
         <h1>Vamos tomar uma?</h1>
       </div>
       <div>
-        <div>
+        <form>
           <input
             type="text"
             placeholder="Login"
             aria-label="Full name"
+            name="email"
+            value={login.email}
             data-testid="common_login__input-email"
+            onChange={ handleChange }
           />
-        </div>
-        <div>
           <input
-            type="text"
+            type="password"
             placeholder="Password"
             aria-label="Full name"
+            name="password"
+            value={ login.password }
             data-testid="common_login__input-password"
+            onChange={ handleChange }
           />
-        </div>
-        <button
-          className="bg-transparent hover:bg-yellow-500
+          <button
+            className="bg-transparent hover:bg-yellow-500
         text-white font-semibold
          hover:text-white py-2 px-4 border border-yellow-400
          hover:border-transparent rounded"
-          type="button"
-          data-testid="common_login__button-login"
-        >
-          Login
-        </button>
-        <button
-          className="bg-transparent hover:bg-yellow-500
+            type="button"
+            name="button"
+            data-testid="common_login__button-login"
+            disabled={ button }
+          >
+            Login
+          </button>
+          <button
+            className="bg-transparent hover:bg-yellow-500
         text-white font-semibold
          hover:text-white py-2 px-4 border border-yellow-400
          hover:border-transparent rounded"
-          type="button"
-          data-testid="common_login__button-register"
-        >
-          Ainda não tenho conta
-        </button>
+            type="button"
+            data-testid="common_login__button-register"
+            name="register"
+          >
+            Ainda não tenho conta
+          </button>
+        </form>
       </div>
     </div>
   );
