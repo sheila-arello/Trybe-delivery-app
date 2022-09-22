46b208cae074677c219885e59df6f51cc7f5e730
André Morkertt <andremorkertt@hotmail.com>
2022-09-22 20:20:55 -0300

    fix: Fix product model

--- back-end/src/database/models/productModel.js
+++ back-end/src/database/models/productModel.js
@@ -8,7 +8,6 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     price: DataTypes.DECIMAL(4, 2),
     urlImage: DataTypes.STRING,
-    role: DataTypes.STRING,
   }, {
     timestamps: false,
     tableName: 'products',
