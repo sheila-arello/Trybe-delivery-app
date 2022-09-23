bc62821a05e2ff7e06d8d4c814878ffeb1bb43c9
Seu nome <halisterfernando@hotmail.com>
2022-09-23 10:59:57 -0300

    build: :construction: valida cadastro

--- front-end/src/pages/register/Register.js
+++ front-end/src/pages/register/Register.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import validate from '../../utils/validate';
 
 export default function Register() {
+  const [register, setRegister] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [button, setButton] = useState(true);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setRegister({ ...register, [name]: value });
+  };
+
+  const validateRegister = () => {
+    const { name, email, password } = register;
+    const isRegisterValid = validate.register(name, email, password);
+
+    if (isRegisterValid) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    validateRegister();
+  }, [register]);
+
   return (
     <div className="cadastro">
       <h2>Cadastro</h2>
@@ -9,20 +36,30 @@ export default function Register() {
           type="text"
           placeholder="Seu nome"
           data-testid="common_register__input-name"
+          name="name"
+          value={ register.name }
+          onChange={ handleChange }
         />
         <input
           type="email"
           placeholder="Seu nome"
           data-testid="common_register__input-email"
+          name="email"
+          value={ register.email }
+          onChange={ handleChange }
         />
         <input
           type="password"
           placeholder="Seu nome"
           data-testid="common_register__input-password"
+          name="password"
+          value={ register.password }
+          onChange={ handleChange }
         />
         <button
           type="submit"
           data-testid="common_register__button-register"
+          disabled={ button }
         >
           Cadastrar
         </button>
