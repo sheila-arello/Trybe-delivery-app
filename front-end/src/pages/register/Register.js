ed0710246f5666f641f7ad358a7c1d73be12aca9
Seu nome <halisterfernando@hotmail.com>
2022-09-23 10:34:22 -0300

    build: :construction: idata test id e demais elementos na página register

--- front-end/src/pages/register/Register.js
+++ front-end/src/pages/register/Register.js
@@ -2,6 +2,37 @@ import React from 'react';
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className="cadastro">
+      <h2>Cadastro</h2>
+      <form>
+        <input
+          type="text"
+          placeholder="Seu nome"
+          data-testid="common_register__input-name"
+        />
+        <input
+          type="email"
+          placeholder="Seu nome"
+          data-testid="common_register__input-email"
+        />
+        <input
+          type="password"
+          placeholder="Seu nome"
+          data-testid="common_register__input-password"
+        />
+        <button
+          type="submit"
+          data-testid="common_register__button-register"
+        >
+          Cadastrar
+        </button>
+      </form>
+    </div>
   );
 }
+
+// common_register__input-name
+// - 7: common_register__input-email
+// - 8: common_register__input-password
+// - 9: common_register__button-register
+// - 10: common_register__element-invalid_register [Elemento oculto (Mensagens de erro)]
