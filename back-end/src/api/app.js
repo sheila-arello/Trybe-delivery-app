5fb041928fce832964d17dff999f6c164446e70e
Gabrielle-Murat <belle.murat@gmail.com>
2022-09-23 00:26:04 -0300

    fix: :bug: conserta erro de lint (de novo)

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const { errorTreatment } = require('../middlewares/errors/general.error');
 require('express-async-errors');
-const cors = require('cors')
 
 const loginRouter = require('../routes/login.routes');
 
 const app = express();
 app.use((cors({
-    origin: '*'
-})))
+    origin: '*',
+})));
 app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
