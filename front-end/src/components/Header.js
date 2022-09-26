997da6b52e93ffc410ddf999820ebb89c8da03ee
Gabrielle-Murat <belle.murat@gmail.com>
2022-09-26 16:08:59 -0300

    refactor: :art: Refatora o header para generalizar

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-function Header({ userType, userName }) {
+function Header({ screenType, userName, userType }) {
+  // console.log(screenType, userType); OK
   return (
     <div>
       <nav>
         <section>
           {
-            userType === 'products'
+            screenType === 'products'
               ? (
                 <span data-testid="customer_products__element-navbar-link-products">
                   PRODUTOS
                 </span>
               )
               : (
-                <span data-testid="customer_products__element-navbar-link-orders">
+                <span data-testid="customer_products__element-navbar-link-products">
                   PEDIDOS
                 </span>
               )
           }
         </section>
+        {
+          userType === 'customer'
+          && (
+            <span data-testid="customer_products__element-navbar-link-orders">
+              MEUS PEDIDOS
+            </span>
+          )
+        }
         <span data-testid="customer_products__element-navbar-user-full-name">
           { userName }
         </span>
@@ -34,8 +43,9 @@ function Header({ userType, userName }) {
 }
 
 Header.propTypes = {
-  userType: propTypes.string.isRequired,
+  screenType: propTypes.string.isRequired,
   userName: propTypes.string.isRequired,
+  userType: propTypes.string.isRequired,
 };
 
 export default Header;
