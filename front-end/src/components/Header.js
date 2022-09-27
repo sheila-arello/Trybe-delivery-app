917560fce621f15aeed1a9eed1a37fdc98ac9185
Seu nome <halisterfernando@hotmail.com>
2022-09-27 15:52:39 -0300

    build: :construction: iniciada construção do carrinho

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-export default function Header({ screenType, userName, userType }) {
+function Header({ screenType, userName, userType }) {
+  // console.log(screenType, userType); OK
+  const handleSubmit = async () => {
+    localStorage.clear();
+    window.location.href = '/login';
+  };
   return (
     <div>
       <nav>
@@ -14,7 +19,7 @@ export default function Header({ screenType, userName, userType }) {
                 </span>
               )
               : (
-                <span data-testid="customer_products__element-navbar-link-products">
+                <span data-testid="customer_products__element-navbar-link-orders">
                   PEDIDOS
                 </span>
               )
@@ -31,11 +36,13 @@ export default function Header({ screenType, userName, userType }) {
         <span data-testid="customer_products__element-navbar-user-full-name">
           { userName }
         </span>
-        <ul>
-          <li data-testid="customer_products__element-navbar-link-logout">
-            Sair
-          </li>
-        </ul>
+        <button
+          type="submit"
+          data-testid="customer_products__element-navbar-link-logout"
+          onClick={ handleSubmit }
+        >
+          Sair
+        </button>
       </nav>
     </div>
   );
@@ -45,3 +52,4 @@ Header.propTypes = {
   userName: propTypes.string.isRequired,
   userType: propTypes.string.isRequired,
 };
+export default Header;
