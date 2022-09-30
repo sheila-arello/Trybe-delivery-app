2a1a8a3d202d440dafd97d17c08c5266c94a3c70
Gabrielle-Murat <belle.murat@gmail.com>
2022-09-30 19:19:35 -0300

    feat: Acrescenta função para rota de update no frontend

--- front-end/src/services/requests.js
+++ front-end/src/services/requests.js
@@ -19,4 +19,9 @@ export const requestPost = async (endpoint, body) => {
   return data;
 };
 
+export const requestPut = async (endpoint, body) => {
+  const { data } = await api.put(endpoint, body);
+  return data;
+};
+
 export default api;
